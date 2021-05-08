879fd92f1d084fb5454e703a66d0db98e9d99795
RoryBr1 <rorybr1@gmail.com>
2021-05-08 18:21:56 +0000

    added mobile navbar. links in main navbar must be updated. recipes_copy (backup file) deleted as not needed.

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-    $(".sidenav").sidenav({
-        edge: "right"
-    });
+    $('.sidenav').sidenav({edge: 'right'});
     $(".tooltipped").tooltip();
     $("select").formSelect();
     $('.modal').modal();
